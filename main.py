55b53d2242b7ad256156af1686b891fa9cf7a269
Jack Drabic <drabicj@moravian.edu>
2024-04-01 19:17:22 -0400

    added imports

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-'''
-Python file for discord!
-'''
+from typing import Final
+import os
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+from  responses import get_response
+
