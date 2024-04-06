96c4ace4a8e13b83d63c2ff218b8dc271f915eb0
Christine Colvin <christinegracecolvin@gmail.com>
2024-04-06 14:28:26 -0400

    updated main

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from typing import Final
 import os
-from dotenv import load_dotenv
+import dotenv
 from discord import Intents, Client, Message
-from  responses import get_response
+from responses import get_response
 
+def main():
+    print('DISCORDTOKEN is found!')
+
+
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+    if not os.getenv('DISCORD_TOKEN'):
+        print('Please set DISCORD_TOKEN in .env file')
+        exit(1)
+    main()
