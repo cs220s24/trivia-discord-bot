70a2e0ec1a231369107691b0f8d9017f86fae03b
RGJ-713 <Rgarcia.dx@gmail.com>
2024-04-06 15:38:15 -0400

    Added bot setup and send_message()

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from typing import Final
 import os
 import dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
 def main():
-    print('DISCORDTOKEN is found!')
 
+    # STEP 1: Bot Setup
+    intents = Intents.default()
+    intents.message_content = True
+    client = Client(intents=intents)
+
+    # STEP 2: Message Functionality
+    def send_message(message, user_message):
+
+        # If no message is provided, return
+        if not user_message:
+            print('No message to send')
+            return
+    
+        # Check if the message is a private message - TRUE or FALSE
+        is_private = user_message.startswith('?')
+
+        # If the message is private, get user message
+        if is_private:
+            user_message = user_message[1:]
+    
+        try:
+
+            # Check for and/or send response
+            response = get_response(user_message)
+
+            if is_private:
+                message.author.send(response)
+
+            else:
+                message.channel.send(response)
+
+        except Exception as e:
+            print(e)
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
