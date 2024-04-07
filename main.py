0f9cf5192cb600404e0170f2ea70c59c4f1c0f7d
Michael Romero <romerom@moravian.edu[core]>
2024-04-07 15:51:18 -0400

    Refactored the code to use discord.py in accordance with a tutorial.

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
-import os
-import dotenv
-from discord import Intents, Client, Message
-from responses import get_response
+# import os
+# import dotenv
+# import discord
+# from responses import get_response
 
-def main():
+# def main():
 
-    # STEP 1: Bot Setup
-    intents = Intents.default()
-    intents.message_content = True
-    client = Client(intents=intents)
+#     # STEP 1: Bot Setup
+
+#     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+#     client = discord.Client()
+
+#     @client.event
+#     async def on_ready():
+#         print(f'{client.user} has connected to Discord!')
+
+#     client.run(DISCORD_TOKEN)
 
     # STEP 2: Message Functionality
-    def send_message(message, user_message):
+    # def send_message(message, user_message):
 
-        # If no message is provided, return
-        if not user_message:
-            print('No message to send')
-            return
+    #     # If no message is provided, return
+    #     if not user_message:
+    #         print('No message to send')
+    #         return
     
-        # Check if the message is a private message - TRUE or FALSE
-        is_private = user_message.startswith('?')
+    #     # Check if the message is a private message - TRUE or FALSE
+    #     is_private = user_message.startswith('?')
 
-        # If the message is private, get user message
-        if is_private:
-            user_message = user_message[1:]
+    #     # If the message is private, get user message
+    #     if is_private:
+    #         user_message = user_message[1:]
     
-        try:
+    #     try:
+    #         # Check for and/or send response
+    #         response = get_response(user_message)
 
-            # Check for and/or send response
-            response = get_response(user_message)
+    #         if is_private:
+    #             message.author.send(response)
 
-            if is_private:
-                message.author.send(response)
+    #         else:
+    #             message.channel.send(response)
 
-            else:
-                message.channel.send(response)
+    #     except Exception as e:
+    #         print(e)
 
-        except Exception as e:
-            print(e)
+# if __name__ == '__main__':
+#     dotenv.load_dotenv()
+#     if not os.getenv('DISCORD_TOKEN'):
+#         print('Please set DISCORD_TOKEN in .env file')
+#         exit(1)
+#     main()
 
-if __name__ == '__main__':
-    dotenv.load_dotenv()
-    if not os.getenv('DISCORD_TOKEN'):
-        print('Please set DISCORD_TOKEN in .env file')
-        exit(1)
-    main()
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
