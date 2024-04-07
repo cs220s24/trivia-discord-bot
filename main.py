ad131bcce2eee25afe7ce53023736a5c4aac8a1c
Christine Colvin <christinegracecolvin@gmail.com>
2024-04-07 15:54:05 -0400

    Updated main within my VScode

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-from typing import Final
+# import os
+# import dotenv
+# import discord
+# from responses import get_response
+
+# def main():
+
+#     # STEP 1: Bot Setup
+
+#     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+
+#     client = discord.Client()
+
+#     @client.event
+#     async def on_ready():
+#         print(f'{client.user} has connected to Discord!')
+
+#     client.run(DISCORD_TOKEN)
+
+    # STEP 2: Message Functionality
+    # def send_message(message, user_message):
+
+    #     # If no message is provided, return
+    #     if not user_message:
+    #         print('No message to send')
+    #         return
+    
+    #     # Check if the message is a private message - TRUE or FALSE
+    #     is_private = user_message.startswith('?')
+
+    #     # If the message is private, get user message
+    #     if is_private:
+    #         user_message = user_message[1:]
+    
+    #     try:
+    #         # Check for and/or send response
+    #         response = get_response(user_message)
+
+    #         if is_private:
+    #             message.author.send(response)
+
+    #         else:
+    #             message.channel.send(response)
+
+    #     except Exception as e:
+    #         print(e)
+
+# if __name__ == '__main__':
+#     dotenv.load_dotenv()
+#     if not os.getenv('DISCORD_TOKEN'):
+#         print('Please set DISCORD_TOKEN in .env file')
+#         exit(1)
+#     main()
+
+# bot.py
 import os
-import dotenv
-from discord import Intents, Client, Message
-from responses import get_response
 
-def main():
-    print('DISCORDTOKEN is found!')
+import discord
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
-if __name__ == '__main__':
-    dotenv.load_dotenv()
-    if not os.getenv('DISCORD_TOKEN'):
-        print('Please set DISCORD_TOKEN in .env file')
-        exit(1)
-    main()
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
+
+client.run(TOKEN)
