05db7895995794e2921b728e09194b8857fd5a47
Michael Romero <romerom@moravian.edu[core]>
2024-04-07 22:11:52 -0400

    Added some questions so the user can say start quiz and the quiz will start! The user will also be told how many they got right and wrong at the end of the quiz.

--- main.py
+++ main.py
@@ -79,8 +79,28 @@ async def on_message(message):
         response = 'pong!'
         await message.channel.send(response)
 
-    if message.content.strip().lower() == 'does my bf like me':
-        response = 'no🤣'
+    if message.content.strip().lower() == 'happy birthday':
+        response = 'Happy Birthday!!'
         await message.channel.send(response)
+
+    quiz_questions = {
+        'Are brownies good? Yes or no': 'Yes',
+        'What country are we currently in?': 'US',
+        'What class is this for?': 'Devops'
+    }
+
+    if message.content.strip().lower() == 'start quiz':
+        num_correct = 0
+        num_incorrect = 0
+        for question, answer in quiz_questions.items():
+            await message.channel.send(question)
+            response = await client.wait_for('message')
+            if response.content.strip().lower() == answer.lower():
+                await message.channel.send('Correct!')
+                num_correct += 1
+            else:
+                await message.channel.send('Incorrect!')
+                num_incorrect += 1
+        await message.channel.send('Quiz complete! You got ' + str(num_correct) + ' correct and ' + str(num_incorrect) + ' incorrect')
 
 client.run(TOKEN)
