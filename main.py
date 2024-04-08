6e7e85f55259b73ae716a33997aa749cae5291e1
Michael Romero <romerom@moravian.edu[core]>
2024-04-07 22:29:53 -0400

    Deleted a bunch of commented out code from before that we were using from a weird tutorial. Also added a time.sleep thing because it was going really fast before, and it seemed like this caused a slight bug

--- main.py
+++ main.py
@@ -1,61 +1,8 @@
-# import os
-# import dotenv
-# import discord
-# from responses import get_response
-
-# def main():
-
-#     # STEP 1: Bot Setup
-
-#     DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-
-#     client = discord.Client()
-
-#     @client.event
-#     async def on_ready():
-#         print(f'{client.user} has connected to Discord!')
-
-#     client.run(DISCORD_TOKEN)
-
-    # STEP 2: Message Functionality
-    # def send_message(message, user_message):
-
-    #     # If no message is provided, return
-    #     if not user_message:
-    #         print('No message to send')
-    #         return
-    
-    #     # Check if the message is a private message - TRUE or FALSE
-    #     is_private = user_message.startswith('?')
-
-    #     # If the message is private, get user message
-    #     if is_private:
-    #         user_message = user_message[1:]
-    
-    #     try:
-    #         # Check for and/or send response
-    #         response = get_response(user_message)
-
-    #         if is_private:
-    #             message.author.send(response)
-
-    #         else:
-    #             message.channel.send(response)
-
-    #     except Exception as e:
-    #         print(e)
-
-# if __name__ == '__main__':
-#     dotenv.load_dotenv()
-#     if not os.getenv('DISCORD_TOKEN'):
-#         print('Please set DISCORD_TOKEN in .env file')
-#         exit(1)
-#     main()
-
 import os
 
 import discord
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -93,6 +40,7 @@ async def on_message(message):
         num_correct = 0
         num_incorrect = 0
         for question, answer in quiz_questions.items():
+            time.sleep(1)
             await message.channel.send(question)
             response = await client.wait_for('message')
             if response.content.strip().lower() == answer.lower():
