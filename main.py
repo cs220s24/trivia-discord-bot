33f1ca8a84e129ebc2a7757e8001cd58faa3a23a
Michael Romero <romerom@moravian.edu[core]>
2024-04-07 22:58:26 -0400

    Changed the dotenv import statement a bit and also put everything into a main() function as a good coding practice. The dotenv.load_dotenv() happens in the if __name__ == '__main__' conditional now.

--- main.py
+++ main.py
@@ -1,73 +1,82 @@
 import os
 import discord
-from dotenv import load_dotenv
+import dotenv
 
 import time
 
-# Load the environment variables
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+def main():
 
-# Create a Discord client
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
-
-# When the bot is ready, print a message to the console
-@client.event
-async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
-
-# When a message is sent, respond with a message if a command is found
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    # Load the environment variables
     
-    # Respond to "ping" with "pong!"
-    if message.content.strip().lower() == 'ping':
-        response = 'pong!'
-        await message.channel.send(response)
+    TOKEN = os.getenv('DISCORD_TOKEN')
+    GUILD = os.getenv('DISCORD_GUILD')
 
-    # Respond to "happy birthday" with "Happy Birthday!!"
-    if message.content.strip().lower() == 'happy birthday':
-        response = 'Happy Birthday!!'
-        await message.channel.send(response)
+    # Create a Discord client
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-    # Respond to "who am i?" with the user's name
-    if message.content.strip().lower() == 'who am i?':
-        response = message.author
-        await message.channel.send(response)
+    client = discord.Client(intents=intents)
 
-    # Respond to "start quiz" with a brief quiz
-    quiz_questions = {
-        'Are brownies good? Yes or no': 'Yes',
-        'What country are we currently in?': 'US',
-        'What class is this for?': 'Devops'
-    }
+    # When the bot is ready, print a message to the console
+    @client.event
+    async def on_ready():
+        print(f'{client.user.name} has connected to Discord!')
 
-    if message.content.strip().lower() == 'start quiz':
-        num_correct = 0
-        num_incorrect = 0
+    # When a message is sent, respond with a message if a command is found
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+    
+        # Respond to "ping" with "pong!"
+        if message.content.strip().lower() == 'ping':
+            response = 'pong!'
+            await message.channel.send(response)
 
-        # Ask each question in the quiz_questions dictionary
-        for question, answer in quiz_questions.items():
-            time.sleep(1)
-            await message.channel.send(question)
+        # Respond to "happy birthday" with "Happy Birthday!!"
+        if message.content.strip().lower() == 'happy birthday':
+            response = 'Happy Birthday!!'
+            await message.channel.send(response)
 
-            response = await client.wait_for('message')
+        # Respond to "who am i?" with the user's name
+        if message.content.strip().lower() == 'who am i?':
+            response = message.author
+            await message.channel.send(response)
 
-            if response.content.strip().lower() == answer.lower():
-                await message.channel.send('Correct!')
-                num_correct += 1
+        # Respond to "start quiz" with a brief quiz
+        quiz_questions = {
+            'Are brownies good? Yes or no': 'Yes',
+            'What country are we currently in?': 'US',
+            'What class is this for?': 'Devops'
+        }
 
-            else:
-                await message.channel.send('Incorrect!')
-                num_incorrect += 1
+        if message.content.strip().lower() == 'start quiz':
+            num_correct = 0
+            num_incorrect = 0
 
-        # Send a message with the number of correct and incorrect answers
-        await message.channel.send('Quiz complete! You got ' + str(num_correct) + ' correct and ' + str(num_incorrect) + ' incorrect.')
+            # Ask each question in the quiz_questions dictionary
+            for question, answer in quiz_questions.items():
+                time.sleep(1)
+                await message.channel.send(question)
 
-client.run(TOKEN)
+                response = await client.wait_for('message')
+
+                if response.content.strip().lower() == answer.lower():
+                    await message.channel.send('Correct!')
+                    num_correct += 1
+
+                else:
+                    await message.channel.send('Incorrect!')
+                    num_incorrect += 1
+
+            # Send a message with the number of correct and incorrect answers
+            await message.channel.send('Quiz complete! You got ' + str(num_correct) + ' correct and ' + str(num_incorrect) + ' incorrect.')
+
+    client.run(TOKEN)
+
+if __name__ == '__main__':
+    dotenv.load_dotenv()
+    if not os.getenv('DISCORD_TOKEN'):
+        print('No token found. Please create a .env file with a DISCORD_TOKEN variable.')
+        exit(1)
+    main()
