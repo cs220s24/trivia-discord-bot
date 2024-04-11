832bf97cb900433f5c6c19dd0d6e654da9d0464e
RGJ-713 <Rgarcia.dx@gmail.com>
2024-04-11 18:45:42 -0400

    Add create.sql and insert.sql

--- database/create.sql
+++ database/create.sql
@@ -1,1 +1,9 @@
--- Hello world
+DROP DATABASE IF EXISTS trivia_db;
+CREATE DATABASE trivia_db;
+USE trivia_db;
+
+CREATE TABLE trivia_questions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255) NOT NULL,
+    answer VARCHAR(255) NOT NULL
+);
--- database/insert.sql
+++ database/insert.sql
@@ -1,1 +1,6 @@
--- Hello world
+INSERT INTO trivia_questions (question, answer) VALUES
+('Are brownies good? Respond "YES" or "NO"', 'Yes'),
+('What country was I created in?', 'US'),
+('What class was I developed for?', 'DevOps'),
+('What is the capital of France?', 'Paris'),
+('What question number is this?', '5');
